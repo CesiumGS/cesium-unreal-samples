f63b65737b4d30cd23a79be71434779fb1c92296
Kevin Ring <kevin@kotachrome.com>
2021-08-02 20:36:02 +1000

    Debugging inability to extract ZIP file.

--- .travis/travis-get-cesium-for-unreal.sh
+++ .travis/travis-get-cesium-for-unreal.sh
@@ -1,7 +1,9 @@
+echo wget "${CESIUM_FOR_UNREAL_BUILD}" -O $PWD/cesium-for-unreal.zip
+echo "start"
 wget "${CESIUM_FOR_UNREAL_BUILD}" -O $PWD/cesium-for-unreal.zip
+echo "done"
 if [[ $TRAVIS_OS_NAME == "windows" ]]
 then
-    # Enable compression because disk space is limited on Travis.
     mkdir -p "C:\Program Files\Epic Games\UE_4.26\Engine\Plugins\Marketplace"
     7z x $PWD/cesium-for-unreal.zip "-oC:\Program Files\Epic Games\UE_4.26\Engine\Plugins\Marketplace"
     rm cesium-for-unreal.zip
