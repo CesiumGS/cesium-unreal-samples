430384806585c0d48dd3d811d7b0128a331dc2cf
Kevin Ring <kevin@kotachrome.com>
2021-08-02 17:58:31 +1000

    Package project on Travis.

---
+++ .travis/travis-get-cesium-for-unreal.sh
@@ -0,0 +1,18 @@
+wget -q ${CESIUM_FOR_UNREAL_BUILD} -O cesium-for-unreal.zip
+if [[ $TRAVIS_OS_NAME == "windows" ]]
+then
+    # Enable compression because disk space is limited on Travis.
+    mkdir "C:\Program Files\Epic Games\UE_4.26\Engine\Plugins\Marketplace"
+    7z x cesium-for-unreal.zip "-oC:\Program Files\Epic Games\UE_4.26\Engine\Plugins\Marketplace"
+    rm cesium-for-unreal.zip
+elif [[ $TRAVIS_OS_NAME == "osx" ]]
+then
+    mkdir -p $HOME/UE_4.26/Engine/Plugins/Marketplace
+    unzip cesium-for-unreal.zip -d $HOME/UE_4.26/Engine/Plugins/Marketplace
+    rm cesium-for-unreal.zip
+elif [[ $TRAVIS_OS_NAME == "linux" ]]
+then
+    mkdir -p $HOME/UE_4.26/Engine/Plugins/Marketplace
+    unzip cesium-for-unreal.zip -d $HOME/UE_4.26/Engine/Plugins/Marketplace
+    rm cesium-for-unreal.zip
+fi
